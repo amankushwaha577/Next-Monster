eaa4c759a3592e013c933748036180c1ad288507
Aman Kushwaha <aman.kushwaha_cs18@gla.ac.in>
2024-02-04 00:25:01 +0530

    server client

---
+++ 12.fetch-data-in-client-server/src/app/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Fetch Data with API in Server Component </h1>
+     <Link href="/productlist">Go to Product Page</Link>
+    </main>
+  )
+}
+
+
